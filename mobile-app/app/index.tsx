316ea05e22c4b9a5706a7b94b508ef9ea5b1e709
Ted Leahy <leahyted@proton.me>
2026-01-25 15:28:05 +0000

    Add react-native-paper

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <PaperProvider>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Text>Edit app/index.tsx to edit this screen.</Text>
+            </View>
+        </PaperProvider>
+    );
 }
